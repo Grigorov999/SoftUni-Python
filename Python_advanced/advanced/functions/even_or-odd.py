a7b184acc9a27356a452248617a39e38aabf1b80
Grigorov999 <70814280+Grigorov999@users.noreply.github.com>
2020-09-11 23:11:47 +0300

    Add files via upload

---
+++ Python_advanced/advanced/functions/even_or-odd.py
@@ -0,0 +1,11 @@
+def even_odd(*args):
+    result = []
+    if args[-1] == 'even':
+        result = [args[i] for i in range(len(args)-1) if args[i] % 2 == 0]
+    elif args[-1] == 'odd':
+        result = [args[i] for i in range(len(args) - 1) if args[i] % 2 != 0]
+    return result
+
+
+print(even_odd(1, 2, 3, 4, 5, 6, "even"))
+print(even_odd(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "odd"))
